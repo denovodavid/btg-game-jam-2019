a8bbdf1753ba95ec1c776fb2a08a335bfb1c0d44
smeagolem <stooge213@hotmail.com>
2019-07-22 16:06:55 +1000

    :bug: fix post processing

--- Assets/TheCamera.cs
+++ Assets/TheCamera.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using Cinemachine;
 
 public class TheCamera : MonoBehaviour
 {
+    public PostProcessVolume postProcessVolume;
+    ChromaticAberration chromaticAberration;
 
-    private void LateUpdate()
+    private void Start()
     {
-        Camera.main.transform.rotation = Quaternion.identity;
+        if (postProcessVolume != null)
+        {
+            chromaticAberration = postProcessVolume.profile.GetSetting<ChromaticAberration>();
+        }
     }
+
     public void Impact()
     {
-        var impulseSource = GetComponent<Cinemachine.CinemachineImpulseSource>();
+        var impulseSource = GetComponent<CinemachineImpulseSource>();
         impulseSource.GenerateImpulse(Vector3.down * 3);
         StartCoroutine(chromaticFlash());
     }
 
     IEnumerator chromaticFlash()
     {
-        var pp = GetComponent<Cinemachine.PostFX.CinemachinePostProcessing>();
-        pp.Profile.GetSetting<ChromaticAberration>().intensity.value = 1f;
+        chromaticAberration.intensity.value = 1f;
         yield return new WaitForSeconds(0.1f);
-        pp.Profile.GetSetting<ChromaticAberration>().intensity.value = 0f;
+        chromaticAberration.intensity.value = 0f;
     }
 }
